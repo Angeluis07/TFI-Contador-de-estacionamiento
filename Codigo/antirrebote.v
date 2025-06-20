d516bc31aa9658d004fc2363135f8492a0c64660
Matias G.C. <matigc046@hotmail.com>
2025-06-20 20:21:44 -0300

    AHI VA EL TP TERMINADO

--- Codigo/antirrebote.v
+++ Codigo/antirrebote.v
@@ -1,4 +1,4 @@
-
+/*
 module antirrebote (
     input clk_slow,       // reloj lento (1 kHz)
     input btn_in,         // botón físico
@@ -15,5 +15,21 @@ module antirrebote (
 
     // Salida estable: solo cambia si el botón fue igual 2 ciclos seguidos
     assign btn_stable = (q1 & q2);
+
+endmodule
+*/
+module antirrebote (
+    input clk_slow,       // reloj lento (1 kHz)
+    input btn_in,         // botón físico
+    output reg btn_stable      // pulso de un ciclo en flanco de bajada
+);
+
+    reg q1, q2;
+
+    always @(posedge clk_slow) begin
+        q1 <= btn_in;
+        q2 <= q1;
+        btn_stable <= ~(q2 & q1); // Pulso de 1 ciclo en flanco de SUBIDA
+    end
 
 endmodule
